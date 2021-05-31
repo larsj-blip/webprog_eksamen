08488ab099e21f4fdcdb81db4fc1a246093bd312
Lars Jaatun <larsjaatun@Larss-MBP.lan>
2021-05-31 10:53:31 +0200

    tester og mer login

---
+++ webapp/tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+
+import pytest
+
+import app
+
+@pytest.fixture
+def client():
+    """ db_fd, app.config['DATABASE'] = tempfile.mkstemp() """
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+            with app.app_context():
+                app.init_db()
+            yield client
+    """ os.close(db_fd)
+    os.unlink(app.config['DATABASE']) """
